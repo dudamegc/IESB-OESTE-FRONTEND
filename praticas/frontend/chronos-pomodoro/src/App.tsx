06b9d83153241c67f9a823191bd35cc177e6e6ba
Maria Eduarda Guimarães Costa <megacosta10@gmail.com>
2026-05-20 10:37:26 -0300

    Construindo estruturação e css

---
+++ praticas/frontend/chronos-pomodoro/src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/theme.css';
+import './styles/global.css';
+import { Heading } from './components/Heading'
+
+function App(){
+    return(
+        <>
+            <Heading/>
+        </>
+    )
+}
+
+export default App;
